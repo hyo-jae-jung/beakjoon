0bb3c5f4e9e1065d85772c1d46edf0db02201250
hyojaejung <hyojaejung@kakao.com>
2021-12-17 00:17:40 +0900

    finish math2

--- 1002.py
+++ 1002.py
@@ -1,5 +1,4 @@
 import sys
-from math import abs
 
 T = int(sys.stdin.readline().strip())
 
@@ -10,8 +9,13 @@ for i in range(T):
     data_list.append([x1,y1,r1,x2,y2,r2])
 
 for i in data_list:
-    if (((x1-x2)**2 + (y1-y2)**2)**0.5 > r1+r2) or (((x1-x2)**2 + (y1-y2)**2)**0.5 <abs(r1-r2)):
-        print(0)
-    elif (((x1-x2)**2 + (y1-y2)**2)**0.5 == r1+r2) or (((x1-x2)**2 + (y1-y2)**2)**0.5 == abs(r1-r2)):
-        print(1)
-    elif 
+    x1,y1,r1,x2,y2,r2 = i
+    if x1 == x2 and y1 == y2 and r1 == r2:
+        print(-1)
+    else:
+        if (((x1-x2)**2 + (y1-y2)**2)**0.5 > r1+r2) or (((x1-x2)**2 + (y1-y2)**2)**0.5 < abs(r1-r2)):
+            print(0)
+        elif (((x1-x2)**2 + (y1-y2)**2)**0.5 == r1+r2) or (((x1-x2)**2 + (y1-y2)**2)**0.5 == abs(r1-r2)):
+            print(1)
+        elif (((x1-x2)**2 + (y1-y2)**2)**0.5 < r1+r2) or (((x1-x2)**2 + (y1-y2)**2)**0.5 > abs(r1-r2)):
+            print(2)
