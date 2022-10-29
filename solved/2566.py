f4155141bc9e2842d2d618b5473e16b677d0a376
hyojaejung <hyojaejung@kakao.com>
2022-10-30 01:29:34 +0900

    2566 has been solved.

---
+++ solved/2566.py
@@ -0,0 +1,10 @@
+table = []
+for _ in range(9):
+    table.append(list(map(int,input().split())))
+
+max_num = max(sum(table,[]))
+
+for i,l in enumerate(table,1):
+    if max_num in l:
+        print(max_num)
+        print("{} {}".format(i,l.index(max_num)+1))
