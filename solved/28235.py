3662b941998e27295e4f01d7a5acac409c726465
hyo-jae-jung <hyojaejung@kakao.com>
2023-07-15 22:53:38 +0900

    28235 has been solved.

---
+++ solved/28235.py
@@ -0,0 +1,8 @@
+from sys import stdin 
+S = stdin.readline().strip()
+d = dict()
+d.update({'SONGDO':'HIGHSCHOOL'})
+d.update({'CODE':'MASTER'})
+d.update({'2023':'0611'})
+d.update({'ALGORITHM':'CONTEST'})
+print(d[S])
