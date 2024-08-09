ee79fba08a6d1a4ffab469e7522013a976a4f6a0
hyo-jae-jung <hyojaejung@kakao.com>
2024-08-09 20:01:27 +0900

    30676 has been solved.

---
+++ solved/30676.py
@@ -0,0 +1,18 @@
+from sys import stdin  
+
+l = int(stdin.readline().strip())
+
+if l >= 620:
+    print("Red")
+elif l >= 590:
+    print("Orange")
+elif l >= 570:
+    print("Yellow")
+elif l >= 495:
+    print("Green")
+elif l >= 450:
+    print("Blue")
+elif l >= 425:
+    print("Indigo")
+elif l >= 380:
+    print("Violet")
