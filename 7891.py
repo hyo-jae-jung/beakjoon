226a31a44a4976ad6578e549f09b76b4ab67174b
hyo-jae-jung <hyojaejung@kakao.com>
2024-10-02 01:49:37 +0900

    all

--- 7891.py
+++
@@ -1,9 +0,0 @@
-from sys import stdin  
-
-t = int(stdin.readline().strip())
-ans = []
-for _ in range(t):
-    x,y = map(int,stdin.readline().strip().split())
-    ans.append(x+y)
-
-print(*ans,sep='\n')
