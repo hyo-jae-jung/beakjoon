1f81a1bed19b2959ee33090bcab080d7d1007dee
hyo-jae-jung <hyojaejung@kakao.com>
2025-01-14 13:18:45 +0900

    update all

--- 23900.py
+++ 23900.py
@@ -4,6 +4,18 @@ from heapq import heapify,heappop
 N = int(stdin.readline().strip())
 A = list(map(int,stdin.readline().strip().split()))
 C = list(map(int,stdin.readline().strip().split()))
+D = A.copy()
+D.sort()
+dd = {}
+
+for i in range(N):
+    dd.update({D[i]:i+1})
+
+for i in range(N):
+    A[i] = dd[A[i]]
+    C[i] = dd[C[i]]
+
+del dd, D
 
 def main():
 
@@ -12,26 +24,24 @@ def main():
 
     global N
     d = {}
-    for i in range(len(A)):
+    for i in range(N):
         d.update({A[i]:i})
 
     B = A.copy()
     B = list(map(lambda x:-x,B))
     heapify(B)
 
-    swap_cnt = 0
-    for i in range(len(A)-1,-1,-1):
+    for i in range(N-1,-1,-1):
         max_idx = d[-heappop(B)]
 
         if i != max_idx:
-            swap_cnt+=1
             d[A[max_idx]] = i
             d[A[i]] = max_idx
             A[max_idx],A[i] = A[i],A[max_idx]
 
         if A == C:
-            return 1
-        
+            return 1   
+
     return 0
 
 print(main())
