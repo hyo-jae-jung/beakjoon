3c66ed387d92493ffac4ecc8d32c5d1c772c2a69
hyo-jae-jung <hyojaejung@kakao.com>
2025-07-14 06:25:24 +0900

    1197 has been solved with MST.

--- solved/1197.py
+++ solved/1197.py
@@ -1,7 +1,37 @@
+def parent_find(parent,x):
+    if parent[x] != x:
+        parent[x] = parent_find(parent,parent[x])
+    return parent[x]
+
+def parent_union(parent,a,b):
+    a = parent_find(parent,a)
+    b = parent_find(parent,b)
+
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
 import sys
-input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-d1 = int(input())
-d2 = int(input())
+V,E = map(int,sys.stdin.readline().strip().split())
 
-print(2*3.141592*d2+2*d1)
+from heapq import heappop,heappush
+h = []
+for _ in range(E):
+    A,B,C = map(int,sys.stdin.readline().strip().split())
+    heappush(h,(C,A,B))
+
+ans = 0
+i = 0
+parent = list(range(V+1))
+
+while h and i < V:
+    cost,a,b = heappop(h)
+    if (a:=parent_find(parent,a)) != (b:=parent_find(parent,b)):
+        parent_union(parent,a,b)
+        ans+=cost
+        i+=1
+
+print(ans)
